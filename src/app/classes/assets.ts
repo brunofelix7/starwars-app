bcdb192030f0c6d4cb3d38b452f46912c759b2d4
brunofelix7 <bruno.felix10@hotmail.com>
2018-07-22 22:27:50 -0300

    inicio da tela de detalhes

---
+++ src/app/classes/assets.ts
@@ -0,0 +1,13 @@
+export class Assets{
+
+    public images: any[];
+
+    constructor(){
+        this.images = [
+            {
+                "luke": "/assets/images/img_luke.jpeg"
+            }
+        ]
+    }
+
+}
--- src/app/classes/people.ts
+++ src/app/classes/people.ts
@@ -4,6 +4,7 @@ export class People {
     public height: string;
     public mass: string;
     public hair_color: string;
+    public skin_color: string;
     public eye_color: string;
     public birth_year: string;
     public gender: string;
